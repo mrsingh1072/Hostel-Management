812e4742091c6b3f68d952ea734532a110f87b36
Saurabh singh <saurabh_kumar@srmap.edu.in>
2026-05-06 20:00:48 +0530

    update

--- index.php
+++ index.php
@@ -46,6 +46,11 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+
+
+
+    
+
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     <title>Hostel Management System</title>
