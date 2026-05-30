fe538c112533a88fd89224d250082af83abe78d7
Saurabh singh <saurabh_kumar@srmap.edu.in>
2026-05-31 01:03:32 +0530

    Update credential

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,7 +1,12 @@
 <?php
-    $dbuser="root";
-    $dbpass="221204";
-    $host="localhost";
-    $db="hostelmsphp";
-    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+$dbuser="if0_42056941";
+$dbpass="Saurabh1100";
+$host="sql301.infinityfree.com";
+$db="if0_42056941_trimurtihostel";
+
+$mysqli = new mysqli($host,$dbuser,$dbpass,$db);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 ?>
